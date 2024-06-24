a103c6d523140c83a93f27de68180d3b8674b415
kasun <88395585+ka0un@users.noreply.github.com>
2024-06-24 10:47:18 +0530

    init

---
+++ src/test/java/com/hapangama/oauth2practise/Oauth2PractiseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hapangama.oauth2practise;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Oauth2PractiseApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
